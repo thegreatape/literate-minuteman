fb18c0d654ebead9941adef5d9de4b99247c5d2d
Thomas Mayfield <tmayfield@patientslikeme.com>
2011-06-25 23:05:19 -0400

    switch to using better search site, only search by titles. doesn't handle AJAX pulling of locations. major WIP. :-)

--- main.rb
+++ main.rb
@@ -4,53 +4,49 @@ require 'open-uri'
 require 'hpricot'
 require 'cgi'
 require 'haml'
+require 'pp'
 
-def by_isbn(isbn)
-  open("http://library.minlib.net/search/?searchtype=i&SORT=D&searcharg=#{isbn}&searchscope=1") do |body|
-    pairs = (Hpricot(body)/'table.bibItems tr.bibItemsEntry').map do |entry|
-      cells = entry/'td'
-      [(cells[0]/'a').inner_html, cells[2].inner_html.sub(/.*&nbsp;/, '').strip]
+def find(title, author) 
+  open(search_url(title, author)) { |body|
+    (Hpricot(body)/'table.browseResult').map do |row|
+      title = (row/'.dpBibTitle').inner_html
+
+      puts row
+      locations = (row/'table.itemTable tr' ).map do |loc|
+        tds = loc/'td'
+        #puts "empty row" if tds.empty?
+        pp [(tds[0]/'a').inner_html.strip, tds[2].inner_html.strip] unless tds.empty?
+        [(tds[0]/'a').inner_html.strip, tds[2].inner_html.strip] unless tds.empty?
+      end
+      puts "empty locations" if locations.empty?
+      puts "--------------"
+      locations = locations.reject {|i| i.nil? || i.empty?}.flatten
+      {:title => title, :locations => Hash[*locations]} 
     end
-    Hash[pairs]
-  end
+  }.reject(&:nil?)
+
 end
 
-def by_title(title) 
-  open(search_url(title)) do |body|
-    pairs = (Hpricot(body)/'.briefcitRow .briefcitTitle a').map do |row|
-      [row.inner_html, row.attributes['href']]
-    end
-    Hash[pairs]
-  end
-
-end
-
-def search_url(title)
-  title = CGI::escape(title)
-  "http://library.minlib.net/search~S1/?searchtype=t&searcharg=#{title}&searchscope=1&SORT=D&extended=0&SUBMIT=Search&searchlimits="
+def search_url(title, author)
+  title = CGI::escape("#{title} / #{author}")
+  "http://find.minlib.net/iii/encore/search/C%7CS#{title}%7COrightresult%7CU1?lang=eng&suite=pearl"
 end
 
 config = YAML::load(File.open('config.yml'))
 Goodreads.configure(config['api_key'])
 client = Goodreads::Client.new
 
-items = client.shelf(config[:user_id], 'to-read')
+#items = client.shelf(config['user_id'], 'to-read')
+items = [client.shelf(config['user_id'], 'to-read').first]
 books = items.map do |entry| 
-  puts "checking #{entry.book.title.strip} with ISBN #{entry.book.isbn}"
-  isbn_results = by_isbn(entry.book.isbn)
-  title_results = []
-  sleep 1
-  unless isbn_results.empty?
-    puts "Found at #{isbn_results.length} branches."
-  else
-    puts "No results by ISBN, checking by title..."
-    title_results = by_title(entry.book.title.strip).keys
-    puts "Found #{title_results.length} possible candidates"
-    sleep 1
-  end
-  {:title => entry.book.title.strip,
-   :by_isbn => isbn_results,
-   :by_title => title_results
+  title = entry.book.title.strip
+  author = entry.book.authors.author.name.strip
+  puts "checking #{title} / #{author} ..."
+  results = find(title, author)
+  puts "Found #{results.length} possible candidates"
+  {:title => title,
+   :author => author,
+   :results => results
   }
 end
 
@@ -58,7 +54,7 @@ haml = Object.new
 engine = Haml::Engine.new(File.read('to-read.haml'))
 engine.def_method(haml, :render, :books, :search_url)
   
-file = File.open(config[:out_file] || 'to-read.html', 'w')
+file = File.open(config['out_file'] || 'to-read.html', 'w')
 file.write(haml.render({
   :books => books, 
   :search_url => lambda  {|i| search_url(i)} 
