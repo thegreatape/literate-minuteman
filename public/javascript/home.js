096988a4194c57480accea2c722b205d3015e2d5
Thomas Mayfield <Thomas.Mayfield@gmail.com>
2011-07-24 17:54:58 -0400

    stylin' a plenty

--- public/javascript/home.js
+++ public/javascript/home.js
@@ -1,8 +1,1 @@
-$(document).ready(function(){
-
-  $('.show').click(function(e){
-    $(e.target).siblings('.locations').toggle();
-    return false;
-  });
-
-});
+$(document).ready(function(){ });
