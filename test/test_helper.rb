268638d09d3378bd6b3866c06a8c69bdcaf40921
Thomas Mayfield <Thomas.Mayfield@gmail.com>
2011-12-03 13:08:26 -0500

    Stub out resque in tests.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,8 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+Resque.stubs(:enqueue)
+
 class ActiveSupport::TestCase
   def fixture_file(path)
     IO.read("#{Rails.root}/test/fixtures/#{path}")
