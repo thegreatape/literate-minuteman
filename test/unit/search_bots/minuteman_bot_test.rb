320efc8780ae528a6d96fbbc1bae13e37f8cb001
Thomas Mayfield <Thomas.Mayfield@gmail.com>
2012-02-11 12:21:37 -0500

    Back to old-new way of getting records, as they seem have fixed the missing locations. Reverts 'Use classic Minuteman record lookup as the newer system seems to be omitting locations entirely.'

--- test/unit/search_bots/minuteman_bot_test.rb
+++ test/unit/search_bots/minuteman_bot_test.rb
@@ -5,28 +5,23 @@ class MinutemanBotTest < ActiveSupport::TestCase
     assert SearchBots::MinutemanBot.new('209303')
   end
   
-  test "looking up simple results via classic record page" do
+  test "looking up simple results without ajax" do
     bot = SearchBots::MinutemanBot.new('209303')
-    uri = "http://find.minlib.net/iii/encore/search/C%7CSGardens+of+the+Moon+Steven+Erikson%7COrightresult%7CU1?lang=eng&suite=pearl"
+    uri = "http://find.minlib.net/iii/encore/search/C__SGardens+of+the+Moon+Steven+Erikson__Orightresult__U1?lang=eng&suite=pearl"
     response = Hashie::Mash.new({
       :body => fixture_file('search_bots/minuteman_bot/gardens_of_the_moon.html')
     })
     bot.stubs(:fetch).with(uri).returns(response)
 
-    ['b2852617', 'b2347273', 'b2236667'].each do |id|
-      record_response = Hashie::Mash.new({
-        :body => fixture_file('search_bots/minuteman_bot/gardens_of_the_moon_records.html')
-      })
-      bot.stubs(:fetch).with("http://library.minlib.net/record=#{id}").returns(record_response)
-    end
-
     results = bot.find("Gardens of the Moon", "Steven Erikson")
-    assert_equal 4, results.length
-    [["Cambridge", "Out"], 
-     ["Needham", "In"], 
-     ["Newton", "Out"], 
-     ["Wellesley", "Out"]].each do |pair|
-      assert results.any? {|i| i[:status] == pair[1] && i[:location] == pair[0]}
+    assert_equal 6, results.length
+    [["In","Waltham"],
+     ["In","Natick"],
+     ["Out","Cambridge"],
+     ["In","Needham"],
+     ["Out","Newton"],
+     ["In","Wellesley"]].each do |pair|
+      assert results.any? {|i| i[:status] == pair[0] && i[:location] == pair[1]}
      end
     
   end
