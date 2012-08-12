5334064a30f733b3760c4c4812d3b734aadb52cd
Thomas Mayfield <Thomas.Mayfield@gmail.com>
2012-08-12 16:10:18 -0400

    WIP: Mostly working lookups for books in Orange County / NC.

--- app/models/search_bots/orange_nc_bot.rb
+++ app/models/search_bots/orange_nc_bot.rb
@@ -9,12 +9,46 @@ module SearchBots
   class OrangeNCBot < AbstractBot
 
     def find(title, author) 
+      # seed the session with the appropriate search
       fetch(ref_url(title, author))
-      fetch(search_url)
+
+      # make an ajax req that'll actually have the search results in it
+      res = fetch(search_url)
+      (Hpricot(res.body) / 'table > tr').map { |tr|
+        availability_link = (tr /  'a.results-title-link-avail').first 
+        next unless availability_link.present?
+        avail = fetch(availability_link.attributes['href'])
+        title = (tr / '.nsm-brief-primary-title-group').first.inner_text.strip
+        author = (tr / '.nsm-brief-primary-author-group').first.inner_text.strip
+        
+        results = []
+        current_loc = ''
+        (Hpricot(avail.body) / 'tr').each do |row|
+          case row.attributes['class']
+          when 'location'
+            current_loc = row.inner_text.strip.gsub(/\n.*$/,'')
+          when 'piece'
+            status = (row / 'td')[-2].inner_text.strip
+            results.push({ status: normalize_status(status),
+                           location: current_loc.clone,
+                           title: "#{title} #{author}" })
+          end
+        end
+        results
+      }.flatten.compact
     end
 
     def search_url
       "http://catalog.co.orange.nc.us/POLARIS/search/components/ajaxResults.aspx?page=1"
+    end
+
+    def normalize_status(str)
+      case str
+      when "In"
+        "In"
+      else
+        "Out"
+      end
     end
 
     def ref_url(title, author)
