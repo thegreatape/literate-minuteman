70e341df6c15f8f2c7e0567f8b6d361640b6089e
Thomas Mayfield <Thomas.Mayfield@gmail.com>
2014-06-29 17:34:50 -0400

    Link back to Goodreads book.

--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -1,4 +1,4 @@
 class BookSerializer < ActiveModel::Serializer
-  attributes :title, :author, :image_url, :id
+  attributes :title, :author, :image_url, :id, :goodreads_link
   has_many :copies
 end
