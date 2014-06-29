7ab748a085325374862a69dab7680d7526892e68
Thomas Mayfield <Thomas.Mayfield@gmail.com>
2014-06-29 10:59:26 -0400

    Add basic auth to resque web interface.

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,4 +1,14 @@
 if ENV["REDISTOGO_URL"].present?
   uri = URI.parse(ENV["REDISTOGO_URL"])
   Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :thread_safe => true)
+
 end
+
+require 'resque/server'
+Resque::Server.class_eval do
+  use Rack::Auth::Basic do |email, password|
+    email == ENV['RESQUE_WEB_HTTP_BASIC_AUTH_USER'] &&
+      password == ENV['RESQUE_WEB_HTTP_BASIC_AUTH_PASSWORD']
+  end
+end
+
