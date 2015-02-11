f4008eab741ceacbef4e6458a8b38ef70bd5a6c8
Thomas Mayfield <Thomas.Mayfield@gmail.com>
2015-02-10 21:28:36 -0500

    Get the app secret tokens out of source.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Minuteman::Application.config.secret_token = '8c8355d87d933a6f7ed8adc5638b443e6ec56b8035bfe3187ca64ab814dac9d189dae3393df5ffb816ab7949606921b91f07746f28ff8e413bfe7fa6a2f1badf'
+Minuteman::Application.config.secret_token = ENV['SECRET_TOKEN']
+Minuteman::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
